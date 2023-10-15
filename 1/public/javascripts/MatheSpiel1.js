10f3b3063ba53a95dafd13f96304bcc1db8f2ad4
ToddHoward42069 <pastowskiphilipp635@gmail.com>
2023-10-15 23:30:07 +0200

    ye

--- 1/public/javascripts/MatheSpiel1.js
+++ 1/public/javascripts/MatheSpiel1.js
@@ -1,29 +1,21 @@
-    // Get elements
-    const answer = document.getElementById("answer");
-    const lives = document.getElementById("lives");
-    const score = document.getElementById("score");
-    const button = document.getElementById("submit");
-    
-    // Initialize variables
-    let scoreInt = 0;
-    let livesInt = 3;
-    
-    // Add event listener to submit button
-    document.querySelector(".submitButtonMathe1").addEventListener("click", () => {
-      // Increment score or decrement lives
-      if (answer.value == 1) {
-        scoreInt += 1;
-      } else {
-        livesInt -= 1;
-      }
-      
-      // Reset score and lives if lives are 0
-      if (livesInt == 0) {
-        scoreInt = 0;
-        livesInt = 3;
-      }
-      
-      // Update HTML elements
-      lives.innerHTML = livesInt;
-      score.innerHTML = scoreInt;
-    });
+let answer = document.getElementById("answer");
+let lives = document.getElementById("lives");
+let score = document.getElementById("score");
+let button = document.getElementById("submit");
+let scoreInt = 0;
+let livesInt = 3;
+
+let submit = document.querySelector(".submitButtonMathe1").addEventListener("click", function () {
+  if (answer.value == 1) {
+    scoreInt += 1;
+  } else {
+    livesInt -= 1;
+  }
+  if (livesInt == 0) {
+    scoreInt = 0;
+    livesInt = 3;
+  }
+  
+  lives.innerHTML = livesInt;
+  score.innerHTML = scoreInt;
+});
