0191d2e021480131d6416fd8eddaeeb55b92e23a
ToddHoward42069 <pastowskiphilipp635@gmail.com>
2023-10-19 00:29:54 +0200

    y

--- 1/public/javascripts/html.js
+++ 1/public/javascripts/html.js
@@ -1,21 +1,21 @@
-// Get references to the elements
-const dropdownMenu = document.querySelector('.dropdown-menu');
-const link = document.querySelector('.link');
-const overlay = document.querySelector('.overlay');
-
-// Function to toggle the dropdown
-function toggleDropdown() {
-    dropdownMenu.classList.toggle('active');
-    overlay.classList.toggle('overlay-active');
-}
-
-// Function to close the dropdown when clicking outside
-function closeDropdown(event) {
-    if (dropdownMenu.classList.contains('active') && !link.contains(event.target)) {
-        toggleDropdown();
-    }
-}
-
-// Add click event listeners
-link.addEventListener('click', toggleDropdown);
-document.addEventListener('click', closeDropdown);
+        // Get references to the elements
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+        const link = document.querySelector('.link');
+        const overlay = document.querySelector('.overlay');
+        
+        // Function to toggle the dropdown
+        function toggleDropdown() {
+            dropdownMenu.classList.toggle('active');
+            overlay.classList.toggle('overlay-active');
+        }
+        
+        // Function to close the dropdown when clicking outside
+        function closeDropdown(event) {
+            if (dropdownMenu.classList.contains('active') && !link.contains(event.target)) {
+                toggleDropdown();
+            }
+        }
+        
+        // Add click event listeners
+        link.addEventListener('click', toggleDropdown);
+        document.addEventListener('click', closeDropdown);
