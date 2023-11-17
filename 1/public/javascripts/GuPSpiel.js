a8445a429ee49a47f7e35fed321b9ab49cbb14a7
ToddHoward42069 <89554916+ToddHoward42069@users.noreply.github.com>
2023-11-17 08:44:21 +0100

    Update GuPSpiel.js

--- 1/public/javascripts/GuPSpiel.js
+++ 1/public/javascripts/GuPSpiel.js
@@ -116,85 +116,85 @@ let makeScene = function () {
     choice2.innerHTML = "Versuche einzugreifen"
     choice3.innerHTML = "..."
    } else if (sceneCode == "b13") {
-      loreOut.innerHTML = "Soldat: Dafür muss sie sterben! Ich lasse dich ihren Weg in die Hölle wählen."
-      choice1.innerHTML = "Hängen"
-      choice2.innerHTML = "Erschießen"
-      choice3.innerHTML = "Zu Tode foltern"
-    } else if (sceneCode == "b131") {
-      loreOut.innerHTML = "Du entscheidest dich dafür, sie zu hängen."
-      choice1.innerHTML = "Vollstrecke das Urteil"
-      choice2.innerHTML = "Zögere und überdenke die Entscheidung"
-      choice3.innerHTML = "Verweigere die Ausführung"
-    } else if (sceneCode == "b1311") {
-      loreOut.innerHTML = "Du vollstreckst das Urteil. Die Gefangene stirbt."
-      choice1.innerHTML = "..."
-      choice2.innerHTML = "..."
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b1312") {
-      loreOut.innerHTML = "Du zögerst und überdenkst die Entscheidung."
-      choice1.innerHTML = "Bleibe standhaft"
-      choice2.innerHTML = "Ändere deine Meinung"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13121") {
-      loreOut.innerHTML = "Du bleibst standhaft und vollstreckst letztendlich das Urteil."
-      choice1.innerHTML = "Fühle Reue"
-      choice2.innerHTML = "Akzeptiere die Entscheidung"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13122") {
-      loreOut.innerHTML = "Du änderst deine Meinung und entscheidest dich, sie zu befreien."
-      choice1.innerHTML = "Begleite sie zur Flucht"
-      choice2.innerHTML = "Bereue die Entscheidung"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b132") {
-      loreOut.innerHTML = "Du entscheidest dich dafür, sie zu erschießen."
-      choice1.innerHTML = "Führe die Exekution durch"
-      choice2.innerHTML = "Zweifle an der Entscheidung"
-      choice3.innerHTML = "Verweigere die Ausführung"
-    } else if (sceneCode == "b1321") {
-      loreOut.innerHTML = "Du führst die Exekution durch. Die Gefangene ist tot."
-      choice1.innerHTML = "..."
-      choice2.innerHTML = "..."
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b1322") {
-      loreOut.innerHTML = "Du zweifelst an deiner Entscheidung und zögerst."
-      choice1.innerHTML = "Halte an deiner Entscheidung fest"
-      choice2.innerHTML = "Ändere die Entscheidung"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13221") {
-      loreOut.innerHTML = "Du behältst deine Entscheidung bei und führst die Exekution durch."
-      choice1.innerHTML = "Empfinde Reue"
-      choice2.innerHTML = "Akzeptiere die Konsequenzen"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13222") {
-      loreOut.innerHTML = "Du änderst deine Meinung und entscheidest dich, sie freizulassen."
-      choice1.innerHTML = "Helfe ihr bei der Flucht"
-      choice2.innerHTML = "Bereue deine Entscheidung"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b133") {
-      loreOut.innerHTML = "Du entscheidest dich dafür, sie zu foltern, bis zum Tod."
-      choice1.innerHTML = "Führe die grausame Tat aus"
-      choice2.innerHTML = "Empfinde Schuldgefühle"
-      choice3.innerHTML = "Verweigere die Ausführung"
-    } else if (sceneCode == "b1331") {
-      loreOut.innerHTML = "Du führst die grausame Tat aus. Die Gefangene stirbt."
-      choice1.innerHTML = "..."
-      choice2.innerHTML = "..."
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b1332") {
-      loreOut.innerHTML = "Du empfindest Schuldgefühle und zögerst."
-      choice1.innerHTML = "Setze die Folter fort"
-      choice2.innerHTML = "Beende die Folter"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13321") {
-      loreOut.innerHTML = "Du setzt die Folter fort bis zum Tod."
-      choice1.innerHTML = "Fühle Reue"
-      choice2.innerHTML = "Akzeptiere die Konsequenzen"
-      choice3.innerHTML = "..."
-    } else if (sceneCode == "b13322") {
-      loreOut.innerHTML = "Du beendest die Folter und empfindest tiefe Schuldgefühle."
-      choice1.innerHTML = "Bereue die Entscheidung"
-      choice2.innerHTML = "Finde eine Möglichkeit zur Wiedergutmachung"
-      choice3.innerHTML = "..."
+    loreOut.innerHTML = "Soldat: Dafür muss sie sterben! Ich lasse dich ihren Weg in die Hölle wählen."
+    choice1.innerHTML = "Hängen"
+    choice2.innerHTML = "Erschießen"
+    choice3.innerHTML = "Zu Tode foltern"
+   } else if (sceneCode == "b131") {
+    loreOut.innerHTML = "Du entscheidest dich dafür, sie zu hängen."
+    choice1.innerHTML = "Vollstrecke das Urteil"
+    choice2.innerHTML = "Zögere und überdenke die Entscheidung"
+    choice3.innerHTML = "Verweigere die Ausführung"
+   } else if (sceneCode == "b1311") {
+    loreOut.innerHTML = "Du vollstreckst das Urteil. Die Gefangene stirbt."
+    choice1.innerHTML = "..."
+    choice2.innerHTML = "..."
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b1312") {
+    loreOut.innerHTML = "Du zögerst und überdenkst die Entscheidung."
+    choice1.innerHTML = "Bleibe standhaft"
+    choice2.innerHTML = "Ändere deine Meinung"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13121") {
+    loreOut.innerHTML = "Du bleibst standhaft und vollstreckst letztendlich das Urteil."
+    choice1.innerHTML = "Fühle Reue"
+    choice2.innerHTML = "Akzeptiere die Entscheidung"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13122") {
+    loreOut.innerHTML = "Du änderst deine Meinung und entscheidest dich, sie zu befreien."
+    choice1.innerHTML = "Begleite sie zur Flucht"
+    choice2.innerHTML = "Bereue die Entscheidung"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b132") {
+    loreOut.innerHTML = "Du entscheidest dich dafür, sie zu erschießen."
+    choice1.innerHTML = "Führe die Exekution durch"
+    choice2.innerHTML = "Zweifle an der Entscheidung"
+    choice3.innerHTML = "Verweigere die Ausführung"
+   } else if (sceneCode == "b1321") {
+    loreOut.innerHTML = "Du führst die Exekution durch. Die Gefangene ist tot."
+    choice1.innerHTML = "..."
+    choice2.innerHTML = "..."
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b1322") {
+    loreOut.innerHTML = "Du zweifelst an deiner Entscheidung und zögerst."
+    choice1.innerHTML = "Halte an deiner Entscheidung fest"
+    choice2.innerHTML = "Ändere die Entscheidung"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13221") {
+    loreOut.innerHTML = "Du behältst deine Entscheidung bei und führst die Exekution durch."
+    choice1.innerHTML = "Empfinde Reue"
+    choice2.innerHTML = "Akzeptiere die Konsequenzen"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13222") {
+    loreOut.innerHTML = "Du änderst deine Meinung und entscheidest dich, sie freizulassen."
+    choice1.innerHTML = "Helfe ihr bei der Flucht"
+    choice2.innerHTML = "Bereue deine Entscheidung"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b133") {
+    loreOut.innerHTML = "Du entscheidest dich dafür, sie zu foltern, bis zum Tod."
+    choice1.innerHTML = "Führe die grausame Tat aus"
+    choice2.innerHTML = "Empfinde Schuldgefühle"
+    choice3.innerHTML = "Verweigere die Ausführung"
+   } else if (sceneCode == "b1331") {
+    loreOut.innerHTML = "Du führst die grausame Tat aus. Die Gefangene stirbt."
+    choice1.innerHTML = "..."
+    choice2.innerHTML = "..."
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b1332") {
+    loreOut.innerHTML = "Du empfindest Schuldgefühle und zögerst."
+    choice1.innerHTML = "Setze die Folter fort"
+    choice2.innerHTML = "Beende die Folter"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13321") {
+    loreOut.innerHTML = "Du setzt die Folter fort bis zum Tod."
+    choice1.innerHTML = "Fühle Reue"
+    choice2.innerHTML = "Akzeptiere die Konsequenzen"
+    choice3.innerHTML = "..."
+   } else if (sceneCode == "b13322") {
+    loreOut.innerHTML = "Du beendest die Folter und empfindest tiefe Schuldgefühle."
+    choice1.innerHTML = "Bereue die Entscheidung"
+    choice2.innerHTML = "Finde eine Möglichkeit zur Wiedergutmachung"
+    choice3.innerHTML = "..."
     sceneCode = "##"
   } else {
     sceneCode = "#"
