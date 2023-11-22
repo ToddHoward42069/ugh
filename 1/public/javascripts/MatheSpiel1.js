4e9ddfc61b1fc08e05a94ba1f8694e076c8ec9eb
ToddHoward42069 <pastowskiphilipp635@gmail.com>
2023-11-22 19:44:52 +0100

    MatheSpiel1 logic changed

--- 1/public/javascripts/MatheSpiel1.js
+++ 1/public/javascripts/MatheSpiel1.js
@@ -1,37 +1,40 @@
-let answer = document.getElementById("answer");
-let lives = document.getElementById("lives");
-let score = document.getElementById("score");
-let question = document.getElementById("question");
-let button = document.getElementById("submit");
+const answer = document.getElementById("answer");
+const lives = document.getElementById("lives");
+const score = document.getElementById("score");
+const question = document.getElementById("question");
+const button = document.getElementById("submit");
 let scoreInt = 0;
 let livesInt = 3;
 let difficulty = "incremental";
 
-// Function to generate random questions based on the selected difficulty level
+// Constants for random number generation
+const EASY_MAX = 20;
+const HARD_MAX = 100;
+const INCREMENTAL_MAX = 10;
+
 function generateRandomQuestion() {
   let operand1, operand2, operator, answer;
 
   switch (difficulty) {
     case "incremental":
-      operand1 = Math.floor(Math.random() * 10) + 1;
-      operand2 = Math.floor(Math.random() * 10) + 1;
+      operand1 = Math.floor(Math.random() * INCREMENTAL_MAX) + 1;
+      operand2 = Math.floor(Math.random() * INCREMENTAL_MAX) + 1;
       operator = ['+', '-', '*', '/'][Math.floor(Math.random() * 4)];
       break;
     case "easy":
-      operand1 = Math.floor(Math.random() * 10) + 1;
-      operand2 = Math.floor(Math.random() * 10) + 1;
+      operand1 = Math.floor(Math.random() * EASY_MAX) + 1;
+      operand2 = Math.floor(Math.random() * operand1) + 1;
       operator = ['+', '-'][Math.floor(Math.random() * 2)];
       break;
     case "hard":
-      operand1 = Math.floor(Math.random() * 100) + 1;
-      operand2 = Math.floor(Math.random() * 100) + 1;
+      operand1 = Math.floor(Math.random() * HARD_MAX) + 1;
+      operand2 = Math.floor(Math.random() * operand1) + 1;
       operator = ['+', '-', '*', '/'][Math.floor(Math.random() * 4)];
       break;
     default:
       break;
   }
 
-  // Calculate the answer based on the generated question
   switch (operator) {
     case '+':
       answer = operand1 + operand2;
@@ -43,15 +46,9 @@ function generateRandomQuestion() {
       answer = operand1 * operand2;
       break;
     case '/':
-      if (operand2 === 0) {
-        operand2 = 1; // Prevent division by zero
-      }
+      operand2 = operand2 === 0 ? 1 : operand2;
       answer = operand1 / operand2;
-
-      // Ensure the answer is either an integer or a multiple of 0.5
-      if (answer % 1 !== 0 && answer % 0.5 !== 0) {
-        return generateRandomQuestion();
-      }
+      answer = Math.round(answer * 100) / 100;
       break;
     default:
       break;
@@ -70,9 +67,14 @@ function displayRandomQuestion() {
 
 displayRandomQuestion();
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
     const playerAnswer = parseFloat(answer.value);
     const correctAnswer = parseFloat(currentQuestion.answer);
+
+    if (isNaN(playerAnswer)) {
+        alert("Please enter a valid number");
+        return;
+    }
 
     if (playerAnswer === correctAnswer) {
         scoreInt += 1;
@@ -90,7 +92,7 @@ button.addEventListener("click", function () {
 });
 
 const restartButton = document.getElementById("restart");
-restartButton.addEventListener("click", function () {
+restartButton.addEventListener("click", () => {
     scoreInt = 0;
     livesInt = 3;
     displayRandomQuestion();
@@ -103,58 +105,39 @@ restartButton.addEventListener("click", function () {
 function gameLost() {
     question.textContent = "Game Over";
     button.disabled = true;
-    // Additional actions after game over
 }
 
-const incrementalButton = document.getElementById("incremental-btn");
-const easyButton = document.getElementById("easy-btn");
-const hardButton = document.getElementById("hard-btn");
+const difficultyButtons = {
+  "incremental-btn": "incremental",
+  "easy-btn": "easy",
+  "hard-btn": "hard"
+};
 
-// Function to remove the "selected" class from all difficulty buttons
-function clearSelectedDifficulty() {
-    const difficultyButtons = document.querySelectorAll(".difficulty-btn");
-    difficultyButtons.forEach(button => {
-        button.classList.remove("selected");
-    });
-}
-
-incrementalButton.addEventListener("click", function () {
-difficulty = "incremental";
-clearSelectedDifficulty();
-incrementalButton.classList.add("selected");
-displayRandomQuestion();
-});
-
-easyButton.addEventListener("click", function () {
-difficulty = "easy";
-clearSelectedDifficulty();
-easyButton.classList.add("selected");
-displayRandomQuestion();
-});
-
-hardButton.addEventListener("click", function () {
-difficulty = "hard";
-clearSelectedDifficulty();
-hardButton.classList.add("selected");
-displayRandomQuestion();
+Object.keys(difficultyButtons).forEach(buttonId => {
+  const button = document.getElementById(buttonId);
+  button.addEventListener("click", () => {
+      difficulty = difficultyButtons[buttonId];
+      Object.values(difficultyButtons).forEach(id => document.getElementById(id).classList.remove("selected"));
+      button.classList.add("selected");
+      displayRandomQuestion();
+  });
 });
 
 const settingsButton = document.getElementById("settings-btn");
 const settingsMenu = document.getElementById("settings-menu");
 
-settingsButton.addEventListener("click", function () {
-settingsMenu.style.display = (settingsMenu.style.display === "none") ? "block" : "none";
+settingsButton.addEventListener("click", () => {
+    settingsMenu.style.display = (settingsMenu.style.display === "none") ? "block" : "none";
 });
 
-// Close the settings menu when clicking outside of it
-window.addEventListener("click", function (event) {
-if (!settingsButton.contains(event.target) && !settingsMenu.contains(event.target)) {
-  settingsMenu.style.display = "none";
-}
+window.addEventListener("click", (event) => {
+    if (!settingsButton.contains(event.target) && !settingsMenu.contains(event.target)) {
+        settingsMenu.style.display = "none";
+    }
 });
 
-answer.addEventListener("keypress", function (event) {
-if (event.keyCode === 13) { // 13 is the key code for Enter
-    button.click();
-}
+answer.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+        button.click();
+    }
 });
